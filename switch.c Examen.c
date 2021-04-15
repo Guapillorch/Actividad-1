1fa2cbf855fe7aef05b92b2b51eec2af9ff7bb41
Guapillorch <82553406+Guapillorch@users.noreply.github.com>
2021-04-14 21:30:15 -0500

    Add files via upload

---
+++ switch.c Examen.c
@@ -0,0 +1,13 @@
+main ()
+{
+    printf ("¿Que tabla de multiplicar deseas?");
+    scanf ("TABLA");
+
+    for (x = i; i = 1; i <= 1000; i++)
+    {
+        printf (TABLA * x);
+    }
+
+    printf ("Gracias por usar esta aplicacion, adios");
+    break;
+}
